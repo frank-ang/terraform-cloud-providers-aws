6beba438c555c28db2057d2b023cff6400c351f0
Frank Ang <frank@thoughtmachine.net>
2025-09-24 15:19:17 +0800

    LICENSE; README update; (#4)

--- aws-cloud/environments/nonprod/outputs.tf
+++ aws-cloud/environments/nonprod/outputs.tf
@@ -2,6 +2,10 @@ output "eks_cluster_name" {
   value = module.eks.cluster_name
 }
 
+output "eks_cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
 output "eks_node_security_group_id" {
   value = module.eks.node_security_group_id
 }
